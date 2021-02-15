06e4a49942e471de1f00b4b898d710ebae05c809
Max Tang <max.tang@outlook.com>
2021-02-15 22:16:02 +1300

    Runable AI

--- clojure-noob/src/clojure_noob/ai.clj
+++ clojure-noob/src/clojure_noob/ai.clj
@@ -18,3 +18,14 @@
                      (vacant? board pos) (not (canWin? (place board pos nextSign) nextSign))
                      :else true)))
             true allPos)))
+
+(defn bestMove [board sign]
+  (def isVacant? (partial vacant? board))
+  (def vacancies (filter isVacant? allPos))
+  (def winningPos (filter (fn [pos]
+                            (def boardAfter (place board pos sign))
+                            (canWin? boardAfter sign)) vacancies))
+  (cond
+    (> (count winningPos) 0) (nth winningPos 0)
+    (> (count vacancies) 0) (nth vacancies 0)
+    :else -1))
--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -12,5 +12,18 @@
                         :else (let [boardAfter (place board pos sign)]
                                 (recur boardAfter (nextPlayerSign sign))))))))
 
+(defn vsAi []
+  (loop [board (initBoard)]
+    (printBoard board)
+    (cond (gameOver? board) (println "GG!")
+          :else (let [pos (. Integer parseInt (read-line))]
+                  (cond (not (vacant? board pos)) (println "Cannot place there ")
+                        :else (let [boardAfter (place board pos 1)]
+                                (cond (gameOver? boardAfter) (println "GG!")
+                                      :else (let []
+                                              (def aiMovePos (bestMove boardAfter 2))
+                                              (def boardAfterAI (place boardAfter aiMovePos 2))
+                                              (recur boardAfterAI)))))))))
+
 (defn -main []
-  (humanGame))
+  (vsAi))
