ae9b8eb6e9fe36eb2dcb3d618aca4dd14242be74
Max Tang <max.tang@outlook.com>
2021-02-16 20:56:32 +1300

    Working minimax AI

--- clojure-noob/src/clojure_noob/ai.clj
+++ clojure-noob/src/clojure_noob/ai.clj
@@ -1,39 +1,54 @@
 (ns clojure-noob.ai
   (:require [clojure-noob.tictactoe :refer :all]))
 
+(defn toPrintLetter [c]
+  (cond
+    (= c 0) "."
+    (= c 1) "x"
+    (= c 2) "o"))
+
+(defn printBoard [board]
+  (println (map toPrintLetter (subvec board 0 3)))
+  (println (map toPrintLetter (subvec board 3 6)))
+  (println (map toPrintLetter (subvec board 6 9)))
+  (println))
+
 (defn nextPlayerSign [sign]
   (cond
     (= sign 1) 2
     (= sign 2) 1))
 
-(defn maxScore [board sign]
-  (def isVacant? (partial vacant? board))
-  (def vacancies (filter isVacant? allPos))
-  (def nextSign (nextPlayerSign sign))
-  (cond
-    (win? board sign) 100
-    (full? board) 0
-    :else (reduce (fn [currentScore pos]
-                    (let [nextBoard1 (place board pos nextSign)]
-                      (min currentScore
-                           (- 0 (maxScore nextBoard1 nextSign)))))
-                  100
-                  vacancies)))
+(defn minimax [board sign]
+  (let [isVacant? (partial vacant? board)
+        vacancies (filter isVacant? allPos)
+        nextSign (nextPlayerSign sign)]
 
-(defn bestMove [board sign]
-  (def isVacant? (partial vacant? board))
-  (def vacancies (filter isVacant? allPos))
-
-  (def scores (map (fn [pos] [(maxScore (place board pos sign) sign) pos]) vacancies))
-  ;; (println scores)
-
-  (second
-   (reduce (fn [best curr]
-             (if (< (nth best 0) (nth curr 0)) curr best))
-           [-200 -1]
-           scores)))
+    (cond (= sign 2) (cond
+                       (win? board nextSign) [-100 -1]
+                       (win? board sign) [100 -1]
+                       (full? board) [0 -1]
+                       :else (reduce (fn [currentScore pos]
+                                       (def nextBoard2 (place board pos sign))
+                                       (def s (minimax nextBoard2 nextSign))
+                                       (println "maximize:" (toPrintLetter sign) [(first s) pos]) (printBoard board)
+                                       (if (< (first currentScore) (first s)) [(first s) pos] currentScore))
+                                     [-100 -1]
+                                     vacancies))
+          :else (cond
+                  (win? board nextSign) [100 -1]
+                  (win? board sign) [-100 -1]
+                  (full? board) [0 -1]
+                  :else (reduce (fn [currentScore pos]
+                                  (let [nextBoard (place board pos sign)]
+                                    (def s (minimax nextBoard nextSign))
+                                    (println "minimize:" (toPrintLetter sign) [(first s) pos]) (printBoard board)
+                                    (if (> (first currentScore) (first s)) [(first s) pos] currentScore)))
+                                [100 -1]
+                                vacancies)))))
 
 (defn aiMove [board sign]
-  (def aiMovePos (bestMove board sign))
-  (def boardAfter (place board aiMovePos sign))
+  (def move (minimax board 2))
+  (println move)
+  (def aiMovePos (second move))
+  (def boardAfter (place board aiMovePos 2))
   boardAfter)
--- clojure-noob/test/clojure_noob/ai_test.clj
+++ clojure-noob/test/clojure_noob/ai_test.clj
@@ -2,12 +2,44 @@
   (:require [clojure.test :refer :all]
             [clojure-noob.ai :refer :all]))
 
-(deftest aiMoveTest
+;; (deftest aiMoveTest
+;;   (testing "Test if current player can win after this move"
+;;     (def board [0 0 0
+;;                 2 1 0
+;;                 0 1 0])
+;;     (is (= (aiMove board 2)
+;;            [0 2 0
+;;             2 1 0
+;;             0 1 0]))))
+
+;; (deftest minimaxTest2
+;;   (testing "Test if current player can win after this move"
+;;     (def board [0 1 0
+;;                 2 1 0
+;;                 2 1 0])
+;;     (is (= (minimax board 2 true 0)
+;;            [-100 -1]))))
+
+;; (deftest minimaxTest3
+;;   (testing "Test if current player can win after this move"
+;;     (def board [0 0 0
+;;                 0 1 0
+;;                 2 1 0])
+;;     (is (= (minimax board 2 true 0)
+;;            [0 1]))))
+
+;; (deftest minimaxTest4
+;;   (testing "Test if current player can win after this move"
+;;     (def board [1 0 0
+;;                 0 0 0
+;;                 0 0 0])
+;;     (is (= (minimax board 2 true 0)
+;;            [0 4]))))
+
+(deftest minimaxTest5
   (testing "Test if current player can win after this move"
-    (def board [0 0 0
-                2 1 0
-                0 1 0])
-    (is (= (aiMove board 2)
-           [0 2 0
-            2 1 0
-            0 1 0]))))
+    (def board [1 2 0
+                0 1 0
+                0 0 0])
+    (is (= (minimax board 2)
+           [-100 8]))))
